70fddaa23e402aa3e109b4520e053d9c1eb3d761
shanin18 <112709243+shanin18@users.noreply.github.com>
2023-02-18 12:15:16 +0600

    added triangle function with validation

--- js/script.js
+++ js/script.js
@@ -9,7 +9,54 @@ for(const card of cards){
     });
 }
 
-// change location function
+// change location
 document.getElementById("btn-blog").addEventListener("click", function(){
     window.location.href = "blog.html";
 });
+
+
+function getInputValue(id){
+    const inputField = document.getElementById(id);
+    const inputFieldValue = inputField.value;
+    inputField.value = "";
+    const inputFieldValueToNumber = parseFloat(inputFieldValue);
+    return inputFieldValueToNumber;
+}
+
+function getTextElementValue(id){
+    const textElementField = document.getElementById(id);
+    const  textElementValue = textElementField.innerText;
+    return textElementValue;
+}
+
+
+document.getElementById("btn-triangle").addEventListener("click", function(){
+    const shapeName = getTextElementValue("triangle");
+    const triangleBaseValue = getInputValue("base");
+    const triangleHeightValue = getInputValue("height");
+    const areaOfTriangle = 0.5 * triangleBaseValue * triangleHeightValue;
+
+    // validation
+    if(isNaN(triangleBaseValue) || isNaN(triangleHeightValue)|| triangleBaseValue < 1 || triangleHeightValue < 1){
+        alert("please provide a valid number");
+        return;
+    }
+    else{
+        // show the data
+        displayData(shapeName, areaOfTriangle);
+    }
+
+});
+
+
+function displayData(shapeName, totalArea, convert){
+    const container = document.getElementById("tbody");
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td>${1}</td>
+    <td>${shapeName}</td>
+    <td>${totalArea}</td>
+    <td>${convert}</td>
+    `;
+    container.appendChild(tr);
+}
